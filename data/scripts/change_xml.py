46a7df7daa63b63589b23e7b08583ad508fc4ee0
a1usha <aleksandr.ushaev@hotmail.com>
2020-10-17 09:31:57 +0700

    scripts

---
+++ data/scripts/change_xml.py
@@ -0,0 +1,26 @@
+from xml.etree import ElementTree
+import os 
+
+folder = 'images'
+path_to_set = 'download_data/downloads/images/'
+path =  "downloads/labels/"
+
+def main(): 
+  
+    for count, filename in enumerate(os.listdir(path)): 
+        
+        tree = ElementTree.parse(path + filename)
+        root = tree.getroot()
+
+        name = root.find("filename").text
+
+        root.find("folder").text = folder
+        root.find("path").text = path_to_set + name
+
+        tree.write(path + filename)
+  
+# Driver Code 
+if __name__ == '__main__': 
+      
+    # Calling main() function 
+    main() 
